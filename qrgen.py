358e9e90c6286ced5afca5fd25742928f424b8ec
TougherMocha93 <aseelama@gitam.in>
2024-02-24 17:54:04 +0530

    first commit

---
+++ qrgen.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import qrcode
+
+def qrgen(email):
+    img = qrcode.make(email)
+    img.save("./Generated/" + email + ".png")
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv('emails.csv')
+
+# Iterate over each row in the DataFrame
+for index, row in df.iterrows():
+    email = row['email']
+    qrgen(email)
